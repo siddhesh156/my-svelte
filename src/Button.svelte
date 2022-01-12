f6c382207daf6a2be98c550902557c45ca1c2b64
Siddhesh <siddhesh.chavan@techno-soft.in>
2022-01-12 18:35:08 +0530

    first commit

---
+++ src/Button.svelte
@@ -0,0 +1,25 @@
+<svelte:options tag="my-button" />
+
+<script>
+import { onMount } from "svelte";
+
+    onMount(()=>{
+        let data = []
+	const fetchSomething = async () => {
+		const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+		const json = await response.json()
+		console.log(json)
+		data = json
+	}
+    fetchSomething()
+    })
+   
+</script>
+<h1>Hello Sevlet</h1>
+<button class="btn">my-button</button>
+
+<style>
+.btn{
+    background: red;
+}
+</style>
