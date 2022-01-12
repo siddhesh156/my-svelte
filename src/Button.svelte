426971f6b7322003f206c793d858418ef4a79428
Siddhesh <siddhesh.chavan@techno-soft.in>
2022-01-12 19:25:17 +0530

    Button comp updated

--- src/Button.svelte
+++ src/Button.svelte
@@ -2,9 +2,10 @@
 
 <script>
 import { onMount } from "svelte";
+let data = []
 
     onMount(()=>{
-        let data = []
+      
 	const fetchSomething = async () => {
 		const response = await fetch('https://jsonplaceholder.typicode.com/posts')
 		const json = await response.json()
@@ -16,6 +17,7 @@ import { onMount } from "svelte";
    
 </script>
 <h1>Hello Sevlet</h1>
+<h3>{data.length > 0 ? data[0].body : null}</h3>
 <button class="btn">my-button</button>
 
 <style>
